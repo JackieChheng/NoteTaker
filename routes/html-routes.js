0a708d00da7ec4f796cca780e2d0ddeaf96e93d7
JackieChheng <jackiechheng4@gmail.com>
2023-12-11 15:26:42 -0700

    html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,2 +1,15 @@
 const path = require('path');
 const router = require('express').Router();
+
+// Home route
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+  
+// Notes route
+  router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+  
+  
+  module.exports = router;
